052468f81ca88eb7a933cfe30f8beafd46e9b805
Arthur Monteiro Tibolla <arthurmonteiro369@gmail.com>
2021-12-27 20:02:14 -0300

    estudos iniciais de jsx

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,12 @@
+import React from "react";
+
 export default function Primeiro() {
-    return "Please, put a smile on this face"
+    const mensage = "const mensage";
+    return (
+        <div>
+            <strong>Please, put a smile on this face <br></br> </strong>
+            <text>I am a 'text' <br></br> {mensage} </text> 
+            
+        </div>
+        )
 }
